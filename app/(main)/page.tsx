2b2389a47dfc41fa9490f7f33f7170ef92a4a676
muse <103637246+musegit88@users.noreply.github.com>
2024-06-01 00:55:12 +0300

    style: remove empty lines

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,6 @@
 import EventsList from "@/components/events-list";
 import Hero from "@/components/hero";
-import { getAllEvents, getNewEvents } from "../actions/event.actions";
-import { Button } from "@/components/ui/button";
+import { getNewEvents } from "../actions/event.actions";
 import ExploreButton from "@/components/ui/explore-button";
 
 export default async function Home() {
@@ -9,7 +8,7 @@ export default async function Home() {
   return (
     <>
       <Hero />
-     
+
       <section className="max-w-7xl lg:mx-auto p-5 xl:px-0 w-full">
         <div className="flex flex-col items-center gap-4 my-4">
           <EventsList
@@ -19,8 +18,12 @@ export default async function Home() {
             emptyDataDescription="please try again"
             page={1}
           />
-          
-          <ExploreButton className="mt-4" label="Find your next event" path="/events"/>
+
+          <ExploreButton
+            className="mt-4"
+            label="Find your next event"
+            path="/events"
+          />
         </div>
       </section>
     </>
