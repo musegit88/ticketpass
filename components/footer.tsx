3d8e83d986070b912543ee4fa26399066319c9fb
Muse <musemy88@gmail.com>
2024-10-16 11:12:00 +0300

    feat: theme selector

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-
 import Image from "next/image";
 import Link from "next/link";
+import { ModeToggle } from "./mode-toggle";
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -10,16 +10,22 @@ const Footer = () => {
     <footer className="border-t">
       <div className="max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full flex flex-col items-center gap-4 md:flex-row md:items-center justify-between">
         <div className="flex flex-col items-center md:flex-row md:items-center gap-4">
-        <Link href={"/"}>
-          <Image src={"/icons/ticketpass.svg"} alt="ticketpass" width={120} height={40}/>
-        </Link>
+          <Link href={"/"}>
+            <Image
+              src={"/icons/ticketpass.svg"}
+              alt="ticketpass"
+              width={120}
+              height={40}
+            />
+          </Link>
           <p className="text-xs text-gray-500">
             {year} Ticketpass. All Rights reserved.
           </p>
         </div>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 text-sm">
           <span>About</span>
           <span>Privacy Policy</span>
+          <ModeToggle />
         </div>
       </div>
     </footer>
