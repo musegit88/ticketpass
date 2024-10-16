6aff2cf4a8cdcae58c7529eecedae5c511fc2a45
Muse <musemy88@gmail.com>
2024-10-16 14:02:36 +0300

    fix: formatting

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -6,7 +6,9 @@ const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="flex-1  max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full">{children}</main>
+      <main className="flex-1  max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full">
+        {children}
+      </main>
       <Footer />
     </div>
   );
