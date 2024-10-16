308ffbb3652a9d0fa0aab78fe79390f6aeb4c57e
Muse <musemy88@gmail.com>
2024-10-16 11:05:03 +0300

    fix: formatting

--- types/types.ts
+++ types/types.ts
@@ -1,155 +1,153 @@
 export type CreateUserProps = {
-    clerkId: string
-    first_name: string
-    last_name: string
-    username: string
-    email: string
-    image_url: string
-}
+  clerkId: string;
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  image_url: string;
+};
 
 export type UpdateUserProps = {
-    first_name: string
-    last_name: string
-    username: string
-    image_url: string
-}
+  first_name: string;
+  last_name: string;
+  username: string;
+  image_url: string;
+};
 
 export type OrderItem = {
-    id: string
-    buyer: string
-    createdAt: Date
-    eventId: string
-    eventTitle: string
-    totalAmount: string
-}
+  id: string;
+  buyer: string;
+  createdAt: Date;
+  eventId: string;
+  eventTitle: string;
+  totalAmount: string;
+};
 
 export type CategoryTypes = {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 };
 
 // Category props for category actions
 export type CreateCategoryProps = {
-    categoryName: string
-}
+  categoryName: string;
+};
 
 export type CreateEventProps = {
-    userId: string
-    event: {
-        title: string
-        categoryId: string
-        description: string
-        location: string
-        isOnline: boolean
-        imageUrl: string
-        isFree: boolean
-        price: string
-        startTime: Date
-        endTime: Date
-        url?: string
-    }
-    path: string
-}
+  userId: string;
+  event: {
+    title: string;
+    categoryId: string;
+    description: string;
+    location: string;
+    isOnline: boolean;
+    imageUrl: string;
+    isFree: boolean;
+    price: string;
+    startTime: Date;
+    endTime: Date;
+    url?: string;
+  };
+  path: string;
+};
 export type UpdateEventProps = {
-    eventId: string
-    userId: string
-    event: {
-        title: string
-        categoryId: string
-        description: string
-        location: string
-        imageUrl: string
-        isFree: boolean
-        price: string
-        startTime: Date
-        endTime: Date
-        url?: string
-    }
-    path: string
-}
+  eventId: string;
+  userId: string;
+  event: {
+    title: string;
+    categoryId: string;
+    description: string;
+    location: string;
+    imageUrl: string;
+    isFree: boolean;
+    price: string;
+    startTime: Date;
+    endTime: Date;
+    url?: string;
+  };
+  path: string;
+};
 
 export type EventProps = {
-    id: string
-    categoryId: string
-    userId: string
-    orderId: string
-    title: string
-    description: string
-    location: string
-    imageUrl: string
-    isFree: boolean
-    price: string
-    startTime: Date
-    endTime: Date
-    url?: string
-    createdAt: Date
-    updatedAt: Date
-    category: CategoryTypes
-    organizer: { id: string, first_name: string, last_name: string }
-}
+  id: string;
+  categoryId: string;
+  userId: string;
+  orderId: string;
+  title: string;
+  description: string;
+  isArchived: boolean;
+  location: string;
+  imageUrl: string;
+  isFree: boolean;
+  price: string;
+  startTime: Date;
+  endTime: Date;
+  url?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  category: CategoryTypes;
+  organizer: { id: string; first_name: string; last_name: string };
+};
 
 export type GetAllEventsParams = {
-    query: string
-    category: string
-    limit?: number
-    page: number | string
-}
-
-
+  query: string;
+  category: string;
+  limit?: number;
+  page: number | string;
+};
 
 export type RelatedEventsByCategoryProps = {
-    categoryId: string,
-    eventId: string,
-    limit?: number,
-    page: number | string
-}
+  categoryId: string;
+  eventId: string;
+  limit?: number;
+  page: number | string;
+};
 
 export type GetEventByUserProps = {
-    userId: string
-    limit?: number
-    page: number
-}
+  userId: string;
+  limit?: number;
+  page: number;
+};
 
 export type CheckoutOrderProps = {
-    eventTitle: string,
-    eventId: string,
-    isFree: boolean,
-    price: string,
-    buyerId: string,
-}
+  eventTitle: string;
+  eventId: string;
+  isFree: boolean;
+  price: string;
+  buyerId: string;
+};
 
 export type CreateOrderProps = {
-    buyerId: string
-    createdAt: Date
-    eventId: string
-    stripeId: string
-    totalAmount: string
-}
+  buyerId: string;
+  createdAt: Date;
+  eventId: string;
+  stripeId: string;
+  totalAmount: string;
+};
 
 export type GetOrdersByUserProps = {
-    userId: string,
-    limit?: number,
-    page: number | string | null
-}
+  userId: string;
+  limit?: number;
+  page: number | string | null;
+};
 
 export type GetOrdersByEventProps = {
-    eventId: string
-    searchText: string
-}
+  eventId: string;
+  searchText: string;
+};
 
 export type SearchParamsProps = {
-    params: { id: string },
-    searchParams: { [key: string]: string | string[] | undefined }
-}
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export type UrlQueryParams = {
-key:string
-params:string
-value:string | null
-}
+  key: string;
+  params: string;
+  value: string | null;
+};
 
 export type RemoveUrlQueryParams = {
-params:string
-keysToRemove:string[]
-}
-
+  params: string;
+  keysToRemove: string[];
+};
